50feae54bf2decb8c136308c851eb299577650f5
fervinicius <viniciusfer263@gmail.com>
2025-07-12 17:02:47 -0300

    feat: add script.js file

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form');
+
+const apiKeyInput = document.getElementById('apiKey');
+
+const gameSelect = document.getElementById('gameSelect');
+
+const questionInput = document.getElementById('questionInput');
+
+const askButton = document.getElementById('askButton');
+
+const aiResponse = document.getElementById('aiResponse');
+
+const sendForm = (event) => {
+  event.preventDefault()
+  console.log(event)
+}
+
+form.addEventListener('submit', sendForm)
